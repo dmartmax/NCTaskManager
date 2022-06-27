6b76bbdb21cfbf51c841a8c056a4749b5af9f95d
Daniela Martínez <dmartmax@hotmail.com>
2022-06-27 13:41:41 -0500

    Practice 1: Class TaskImpl. The nextTimeAfter method was completed. In previous version, not all possible cases were being considered. Java docs were also added to explain the methods.

--- src/main/java/mx/tc/j2se/tasks/TaskImpl.java
+++ src/main/java/mx/tc/j2se/tasks/TaskImpl.java
@@ -12,9 +12,18 @@ public class TaskImpl implements Task{
 
 
     //Constructors
-    public TaskImpl(){}
+    public TaskImpl(){
+        /**
+         * This constructor builds a task without any properties.
+         */
+    }
 
     public TaskImpl(String title, int time){
+        /**
+         * This constructor builds an inactive, non-repetitive task. In order to do this,
+         * it needs a title (string) for the task to build and the beginning time.
+         * This number needs to be positive integer, because it refers to time.
+         */
         this.title = title;
         this.time = time;
         repeat=false;
@@ -22,6 +31,11 @@ public class TaskImpl implements Task{
     }
 
     public TaskImpl(String title, int start, int end, int interval){
+        /**
+         * This constructor builds an inactive, repetitive task. In order to do this, it needs a title (string)
+         * for the task. Also, it needs the date for starting and ending the task, as well as an interval
+         * expressed in hours. All of these numbers need to be positive integers, because they refer to time.
+         */
         this.title = title;
         this.start = start;
         this.end = end;
@@ -33,65 +47,105 @@ public class TaskImpl implements Task{
 
     // Methods
     public String getTitle(){
+        /**
+         * This method return the title of the task, given as a string.
+         */
         return title;
     }
 
      public void setTitle(String title){
+         /**
+          * This method allows to give a name to the task. The title must be given as a string.
+          */
         this.title = title;
     }
 
     public boolean isActive() {
+        /**
+         * This method returns the status of the task, as a boolean.
+         * True means the task is active. False means the task is inactive.
+         */
         return active;
     }
 
     public void setActive(boolean active) {
+        /**
+         * This method allows to change the task status, to active or inactive.
+         * The value must be given as a boolean. True for active, false for inactive.
+         */
         this.active = active;
     }
 
     public int getTime() {
-        if (repeat==false){
-            return time;
+        /**
+         * If the task is repetitive, this method returns the time when the task starts.
+         * If the task is non-repetitive, it returns the time of execution.
+         */
+        if (repeat){
+            return start;
         }
         else{
-            return start;
+            return time;
         }
 
     }
 
     public void setTime(int time) {
+        /**
+         * This method allows to set the time of execution for a non-repetitive task. The time
+         * given must be a positive integer. If the task was built as repetitive, this method
+         * will change it to a non-repetitive one.
+         */
         this.time = time;
         repeat=false;
 
     }
 
     public int getStartTime() {
-        if (repeat==false){
-            return time;
+        /**
+         * If the task is repetitive, this method returns the time when the task starts.
+         * If the task is non-repetitive, it returns the time of execution.
+         */
+        if (repeat){
+            return start;
         }
         else{
-            return start;
+            return time;
         }
     }
 
     public int getEndTime() {
-        if (repeat==false){
-            return time;
+        /**
+         * If the task is repetitive, this method returns the time when the task ends.
+         * If the task is non-repetitive, it returns the time of execution.
+         */
+        if (repeat){
+            return end;
         }
         else{
-            return end;
+            return time;
         }
     }
 
     public int getRepeatInterval() {
-        if (repeat==false){
-            return 0;
+        /**
+         * If the task is repetitive, this method returns the repeat interval expressed in hours.
+         * If the task is non-repetitive, it returns 0.
+         */
+        if (repeat){
+            return interval;
         }
         else{
-            return interval;
+            return 0;
         }
     }
 
     public void setTime(int start, int end, int interval) {
+        /**
+         * This method allows to set the time for starting and ending the task, as well as the repeat
+         * interval, for repetitive tasks. All of these values must be given as positive integers.
+         * If the task was built as non-repetitive, this method will change it to a repetitive one.
+         */
         this.start = start;
         this.end = end;
         this.interval = interval;
@@ -100,12 +154,33 @@ public class TaskImpl implements Task{
     }
 
     public boolean isRepeated() {
+        /**
+         * This method allows to know if the task is repetitive or non-repetitive
+         * It returns a boolean. A true value means the task is repetitive.
+         * A false value means the task is non-repetitive.
+         */
         return repeat;
     }
 
     public int nextTimeAfter(int current) {
-        if (active==true){
-            if (repeat==false){
+        /**
+         * This method allows to know the nearest moment when the task is going to be executed,
+         * for a current given time, which needs to be a positive integer. For this, the task
+         * needs to be active. If not, the return value will be -1.
+         *
+         * If the task is non-repetitive and the current time is located before the execution time,
+         * then the method will return the time value. If current time is located after execution time
+         * it will return -1, since the task has already been completed.
+         *
+         * If the task is repetitive and the current time occurs before the starting time, the method will
+         * return the starting time. If current time occurs after or at the same time that the ending time,
+         * the method will return -1. However, if the current time is located between the starting and
+         * ending time, the method will return the nearest moment when the task is going to be executed,
+         * according to the given interval.
+         */
+
+        if (active){
+            if (!repeat){
                 if(current<time){
                     return time;
                 }
@@ -118,16 +193,23 @@ public class TaskImpl implements Task{
                 if(current<start){
                     return start;
                 }
-                else if (current>=start && current<end){
-                    return start+interval;
+                else if (start<current && current<end) {
+                    int next=start;
+                    while (next<current){
+                        next+=interval;
+                    }
+                    if (next>=end){
+                        return -1;
+                    }
+                    else {
+                        return next;
+                    }
                 }
                 else{
                     return -1;
                 }
             }
-
         }
-
         else{
             return -1;
         }
